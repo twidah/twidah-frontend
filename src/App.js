16c140b674223ec1ef4a334365f4081e4c09cf13
Berenika Ahmetaj <ahmetajberenika@gmail.com>
2022-03-17 22:21:57 -0400

    Added routes from homepage , signup and login page

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import "./App.css";
 import LoginForm from "./comps/login/Login";
 import RegisterForm from "./comps/register/Register";
 import Header from "./comps/Header";
+import { Route, Routes } from "react-router-dom";
+// import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div>
-      <Header />
-      <RegisterForm />
-      <LoginForm />
+      <Routes>
+        <Route path="/" element={<Header />}></Route>
+        <Route path="/register" element={<RegisterForm />}></Route>
+        <Route path="/signup" element={<LoginForm />}></Route>
+      </Routes>
     </div>
   );
 }
