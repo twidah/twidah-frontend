4200e3654fc40769fa01ddd53c914255083faade
Ant Man <34754995+aaronamendez@users.noreply.github.com>
2022-03-22 19:48:46 -0400

    Gave the signup page some spacing for styling, confirmed website works at www.twidah.com

--- src/configs/api.js
+++ src/configs/api.js
@@ -1,5 +1,5 @@
 export const LoginAPI =
-  process.env.API_BASE_URL || "http://localhost:4000/api/auth/login";
+  process.env.API_LOGIN || "http://localhost:4000/api/auth/login";
 
 export const SignUpAPI =
   process.env.API_SIGNUP || "http://localhost:4000/api/auth/register";
