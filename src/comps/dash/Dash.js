654ead207af6bdc8660eb7374349c0ab9494bd31
Ant Man <34754995+aaronamendez@users.noreply.github.com>
2022-03-23 13:41:32 -0400

    Layout of dash set.

--- src/comps/dash/Dash.js
+++ src/comps/dash/Dash.js
@@ -5,25 +5,13 @@ export const Dash = () => {
     <>
       <div className="wrapper">
         <div>
-          Excepteur id proident reprehenderit reprehenderit velit mollit Lorem
-          ex magna anim quis labore non. Non qui esse aliquip cillum. Veniam
-          fugiat enim ipsum nisi amet sunt reprehenderit ullamco aute irure.
-          Lorem culpa in elit labore deserunt labore duis Lorem adipisicing aute
-          eu. Dolor duis in elit ad pariatur exercitation dolore. Proident
-          occaecat magna officia officia nostrud tempor culpa consequat ea
-          pariatur laborum reprehenderit aliqua irure.
+          <h1>Side Personal Nav</h1>
         </div>
         <div>
-          Excepteur id proident reprehenderit reprehenderit velit mollit Lorem
-          ex magna anim quis labore non. Non qui esse aliquip cillum. Veniam
-          fugiat enim ipsum nisi amet sunt reprehenderit ullamco aute irure.
-          Lorem culpa in elit labore deserunt.
+          <h1>Feed Component</h1>
         </div>
         <div>
-          Excepteur id proident reprehenderit reprehenderit velit mollit Lorem
-          ex magna anim quis labore non. Non qui esse aliquip cillum. Veniam
-          fugiat enim ipsum nisi amet sunt reprehenderit ullamco aute irure.
-          Lorem culpa in elit labore deserunt.
+          <h1>Notify Nav</h1>
         </div>
       </div>
     </>
