de8990f9a792fa0ebcb55d54524903e296395031
Ant Man <34754995+aaronamendez@users.noreply.github.com>
2022-03-24 15:47:43 -0400

    Finally ... lol particles was a bitch to setup.

--- src/App.js
+++ src/App.js
@@ -8,55 +8,63 @@ import { Dash } from "./comps/dash/Dash";
 import { Route, Routes } from "react-router-dom";
 import { RequireAuth, PublicAuth } from "./comps/Auth";
 
+import { ParticlesBackground } from "./ParticlesBackground";
+
 function App() {
-  const [logged, setLogged] = useState(false);
-  useEffect(() => {
-    const isLoggedIn = localStorage.getItem("token");
-    if (isLoggedIn) {
-      setLogged(true);
-    } else {
-      setLogged(false);
-    }
-  }, []);
-  return (
-    <div>
-      <Header logged={logged} />
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <RequireAuth redirectTo="/login">
-              <Dash />
-            </RequireAuth>
-          }
-        />
-        <Route
-          path="/signup"
-          element={
-            <PublicAuth redirectTo="/">
-              <SignUp setLogged={setLogged} />
-            </PublicAuth>
-          }
-        />
-        <Route
-          path="/login"
-          element={
-            <PublicAuth redirectTo="/">
-              <LoginForm setLogged={setLogged} />
-            </PublicAuth>
-          }
-        />
-        <Route
-          path="/logout"
-          element={
-            <RequireAuth redirectTo="/login">
-              <Logout setLogged={setLogged} />
-            </RequireAuth>
-          }
-        />
-      </Routes>
-    </div>
-  );
+    const [logged, setLogged] = useState(false);
+    useEffect(() => {
+        const isLoggedIn = localStorage.getItem("token");
+        if (isLoggedIn) {
+            setLogged(true);
+        } else {
+            setLogged(false);
+        }
+    }, []);
+
+    return (
+        <div className="app">
+            <div id="particles">
+                <ParticlesBackground />
+            </div>
+            <div className="main">
+                <Header logged={logged} />
+                <Routes>
+                    <Route
+                        path="/"
+                        element={
+                            <RequireAuth redirectTo="/login">
+                                <Dash />
+                            </RequireAuth>
+                        }
+                    />
+                    <Route
+                        path="/signup"
+                        element={
+                            <PublicAuth redirectTo="/">
+                                <SignUp setLogged={setLogged} />
+                            </PublicAuth>
+                        }
+                    />
+                    <Route
+                        path="/login"
+                        element={
+                            <PublicAuth redirectTo="/">
+                                <LoginForm setLogged={setLogged} />
+                            </PublicAuth>
+                        }
+                    />
+                    <Route
+                        path="/logout"
+                        element={
+                            <RequireAuth redirectTo="/login">
+                                <Logout setLogged={setLogged} />
+                            </RequireAuth>
+                        }
+                    />
+                </Routes>
+            </div>
+        </div>
+    );
 }
 
 export default App;
--- src/comps/login/Login.js
+++ src/comps/login/Login.js
@@ -6,92 +6,91 @@ import * as yup from "yup";
 import { loginSchema } from "../../validations/LoginSchema";
 
 const loginFormState = {
-  usernameOrEmail: "",
-  password: "",
+    usernameOrEmail: "",
+    password: "",
 };
 
 const initialErrorState = {
-  usernameOrEmail: "",
-  password: "",
+    usernameOrEmail: "",
+    password: "",
 };
 
 const initialDisabled = true;
 
 export default function LoginForm({ setLogged }) {
-  const [login, setLogin] = useState(loginFormState);
-  const [errors, setErrors] = useState(initialErrorState);
-  const [disabled, setDisabled] = useState(initialDisabled);
+    const [login, setLogin] = useState(loginFormState);
+    const [errors, setErrors] = useState(initialErrorState);
+    const [disabled, setDisabled] = useState(initialDisabled);
 
-  let navigate = useNavigate();
+    let navigate = useNavigate();
 
-  const validate = (name, value) => {
-    yup
-      .reach(loginSchema, name)
-      .validate(value)
-      .then(() => setErrors({ ...errors, [name]: "" }))
-      .catch((err) => setErrors({ ...errors, [name]: err.errors[0] }));
-  };
-
-  const change = (e) => {
-    const { name, value } = e.target;
-    validate(name, value);
-    setLogin({ ...login, [name]: value });
-  };
-
-  const submit = (e) => {
-    e.preventDefault();
-    const obj = {
-      usernameOrEmail: login.usernameOrEmail,
-      password: login.password,
+    const validate = (name, value) => {
+        yup.reach(loginSchema, name)
+            .validate(value)
+            .then(() => setErrors({ ...errors, [name]: "" }))
+            .catch((err) => setErrors({ ...errors, [name]: err.errors[0] }));
     };
-    axios
-      .post("https://twidah.herokuapp.com/api/auth/login", obj)
-      .then((res) => {
-        localStorage.setItem("token", `Bearer ${res.data.token}`);
-        setLogged(true);
-        navigate("/");
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  };
 
-  useEffect(() => {
-    loginSchema.isValid(login).then((valid) => setDisabled(!valid));
-  }, [login]);
+    const change = (e) => {
+        const { name, value } = e.target;
+        validate(name, value);
+        setLogin({ ...login, [name]: value });
+    };
 
-  return (
-    <form onSubmit={submit}>
-      <span>{errors.usernameOrEmail}</span>
-      <br />
-      <span>{errors.password}</span>
-      <br />
-      <label htmlFor="usernameOrEmail">
-        {""}
-        Username/Email
-        <input
-          type="text"
-          name="usernameOrEmail"
-          placeholder="Enter Username/Email"
-          value={login.usernameOrEmail}
-          onChange={change}
-        />
-      </label>
-      <br />
+    const submit = (e) => {
+        e.preventDefault();
+        const obj = {
+            usernameOrEmail: login.usernameOrEmail,
+            password: login.password,
+        };
+        axios
+            .post("https://twidah.herokuapp.com/api/auth/login", obj)
+            .then((res) => {
+                localStorage.setItem("token", `Bearer ${res.data.token}`);
+                setLogged(true);
+                navigate("/");
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    };
 
-      <label htmlFor="password">
-        {""}
-        Password
-        <input
-          type="password"
-          name="password"
-          placeholder="Enter Password"
-          value={login.password}
-          onChange={change}
-        />
-      </label>
-      <br />
-      <button disabled={disabled}>Login</button>
-    </form>
-  );
+    useEffect(() => {
+        loginSchema.isValid(login).then((valid) => setDisabled(!valid));
+    }, [login]);
+
+    return (
+        <form onSubmit={submit}>
+            <span>{errors.usernameOrEmail}</span>
+            <br />
+            <span>{errors.password}</span>
+            <br />
+            <label htmlFor="usernameOrEmail">
+                {""}
+                Username/Email
+                <input
+                    type="text"
+                    name="usernameOrEmail"
+                    placeholder="Enter Username/Email"
+                    value={login.usernameOrEmail}
+                    onChange={change}
+                />
+            </label>
+            <br />
+
+            <label htmlFor="password">
+                {""}
+                Password
+                <input
+                    type="password"
+                    name="password"
+                    placeholder="Enter Password"
+                    value={login.password}
+                    onChange={change}
+                />
+            </label>
+            <br />
+            <button disabled={disabled}>Login</button>
+        </form>
+    );
 }
