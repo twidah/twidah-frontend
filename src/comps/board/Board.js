e3148053b979c86220d9e6387e3989716dc78789
Ant Man <34754995+aaronamendez@users.noreply.github.com>
2022-03-24 13:46:11 -0400

    Setup the feed.

--- src/comps/board/Board.js
+++ src/comps/board/Board.js
@@ -1,12 +1,22 @@
 import React from "react";
-import { Twoot } from "./twoot/Twoot";
 import { Routes, Route } from "react-router-dom";
+import { Twoot } from "./twoot/Twoot";
+import { Feed } from "./feed/Feed";
 
 export const Board = () => {
     return (
         <>
             <Routes>
-                <Route path="/" element={<Twoot />} />
+                <Route
+                    path="/"
+                    element={
+                        <>
+                            <Twoot />
+                            <Feed />
+                        </>
+                    }
+                />
+                {/* <Route path="/" element={} /> */}
             </Routes>
         </>
     );
---
+++ src/comps/board/feed/Feed.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Feed = () => {
+    return (
+        <div>
+            <h1>Feed</h1>
+        </div>
+    );
+};
