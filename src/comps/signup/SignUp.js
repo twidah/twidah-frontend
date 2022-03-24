efbd81d90c23e6b291aa2158e785c70b5b9aace6
Ant Man <34754995+aaronamendez@users.noreply.github.com>
2022-03-24 17:42:38 -0400

    Made the login and signup a bit more pleasant to work with.

--- src/comps/signup/SignUp.js
+++ src/comps/signup/SignUp.js
@@ -2,125 +2,118 @@ import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 
+import "../form.css";
+
 import * as yup from "yup";
 import { signupSchema } from "../../validations/SignupSchema";
 
 const initialFormState = {
-  username: "",
-  email: "",
-  password: "",
+    username: "",
+    email: "",
+    password: "",
 };
 
 const initialErrorState = {
-  username: "",
-  email: "",
-  password: "",
+    username: "",
+    email: "",
+    password: "",
+};
+
+const initialFetchErrors = {
+    message: "",
 };
 
 const initialDisabled = true;
 
 export default function SignUp({ setLogged }) {
-  const [form, setForm] = useState(initialFormState);
-  const [errors, setErrors] = useState(initialErrorState);
-  const [disabled, setDisabled] = useState(initialDisabled);
-  const [fetchErrors, setFetchErrors] = useState("");
+    const [form, setForm] = useState(initialFormState);
+    const [errors, setErrors] = useState(initialErrorState);
+    const [disabled, setDisabled] = useState(initialDisabled);
+    const [fetchErrors, setFetchErrors] = useState(initialFetchErrors);
 
-  const validate = (name, value) => {
-    yup
-      .reach(signupSchema, name)
-      .validate(value)
-      .then(() => setErrors({ ...errors, [name]: "" }))
-      .catch((err) => setErrors({ ...errors, [name]: err.errors[0] }));
-  };
-
-  const change = (e) => {
-    const { name, value } = e.target;
-    validate(name, value);
-    setForm({ ...form, [name]: value });
-  };
-
-  let navigate = useNavigate();
-
-  const submit = (e) => {
-    e.preventDefault();
-
-    const obj = {
-      username: form.username,
-      email: form.email,
-      password: form.password,
+    const validate = (name, value) => {
+        yup.reach(signupSchema, name)
+            .validate(value)
+            .then(() => setErrors({ ...errors, [name]: "" }))
+            .catch((err) => setErrors({ ...errors, [name]: err.errors[0] }));
     };
-    axios
-      .post("https://twidah.herokuapp.com/api/auth/register", obj)
-      .then((res) => {
-        localStorage.setItem("token", `Bearer ${res.data.token}`);
-        setLogged(true);
-        navigate("/");
-      })
-      .catch((err) => setFetchErrors(err));
-  };
 
-  useEffect(() => {
-    signupSchema.isValid(form).then((valid) => setDisabled(!valid));
-  }, [form]);
+    const change = (e) => {
+        const { name, value } = e.target;
+        validate(name, value);
+        setForm({ ...form, [name]: value });
+    };
 
-  return (
-    <form onSubmit={submit}>
-      <span>{fetchErrors}</span>
-      <span>{errors.username}</span>
-      <br />
-      <span>{errors.email}</span>
-      <br />
-      <span>{errors.password}</span>
-      <br />
-      <label htmlFor="username">
-        {" "}
-        Username
-        <input
-          type="text"
-          name="username"
-          placeholder="Enter your username"
-          value={form.username}
-          onChange={change}
-        />
-      </label>
-      <br />
-      <label htmlFor="email">
-        {""}
-        Email
-        <input
-          type="email"
-          name="email"
-          placeholder="Enter your email"
-          value={form.email}
-          onChange={change}
-        />
-      </label>
-      <br />
-      <label htmlFor="password">
-        {""}
-        Password
-        <input
-          type="password"
-          name="password"
-          placeholder="Enter your password"
-          value={form.password}
-          onChange={change}
-        />
-      </label>
-      <br />
-      <button
-        disabled={disabled}
-        style={{
-          cursor: "pointer",
-          marginLeft: "5px",
-          fontSize: "18px",
-          backgroundColor: "#69db7c",
-          color: "white",
-        }}
-        type="submit"
-      >
-        Sign up!
-      </button>
-    </form>
-  );
+    let navigate = useNavigate();
+
+    const submit = (e) => {
+        e.preventDefault();
+
+        const obj = {
+            username: form.username,
+            email: form.email,
+            password: form.password,
+        };
+        axios
+            .post("https://twidah.herokuapp.com/api/auth/register", obj)
+            .then((res) => {
+                localStorage.setItem("token", `Bearer ${res.data.token}`);
+                setLogged(true);
+                navigate("/");
+            })
+            .catch(({ response }) =>
+                setFetchErrors({
+                    ...fetchErrors,
+                    message: response.data.message,
+                })
+            );
+    };
+
+    useEffect(() => {
+        signupSchema.isValid(form).then((valid) => setDisabled(!valid));
+    }, [form]);
+
+    return (
+        <div className="form">
+            <form onSubmit={submit}>
+                <span>{errors.username}</span>
+                <br />
+                <span>{errors.email}</span>
+                <br />
+                <span>{errors.password}</span>
+                <br />
+                <label htmlFor="username">Username</label>
+                <br />
+                <input
+                    type="text"
+                    name="username"
+                    placeholder="Enter Username"
+                    value={form.username}
+                    onChange={change}
+                />
+                <br />
+                <label htmlFor="email">Email</label>
+                <br />
+                <input
+                    type="email"
+                    name="email"
+                    placeholder="Enter Email"
+                    value={form.email}
+                    onChange={change}
+                />
+                <br />
+                <label htmlFor="password">Password</label>
+                <br />
+                <input
+                    type="password"
+                    name="password"
+                    placeholder="Enter Password"
+                    value={form.password}
+                    onChange={change}
+                />
+                <br />
+                <button disabled={disabled}>Sign up!</button>
+            </form>
+        </div>
+    );
 }
--- src/config/particles-config.js
+++ src/config/particles-config.js
@@ -1,7 +1,7 @@
 export const particlesConfig = {
     particles: {
         number: {
-            value: 80,
+            value: 21,
             density: {
                 enable: true,
                 value_area: 800,
